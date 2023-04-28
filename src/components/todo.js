6ee8b4c09943022fb9c0474a74d41297d73b155b
Radif Elnoun <SIRIOSUKUR@GMAIL.COM>
2023-04-28 20:08:53 +0200

    finished localstorage

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,47 +1,63 @@
-import {useState} from "react"
+import { useState } from "react"
 
-export default function Todo({ item, onUpdate, onDelete}) {
+export default function Todo({ item, onUpdate, onDelete }) {
   const [isEdit, setIsEdit] = useState(false);
 
-    function FormEdit() {
+  function FormEdit() {
 
-        const [newValue, setNewValue] = useState(item.title)
+    const [newValue, setNewValue] = useState(item.title)
 
-        function handleSubmit(e) {
-            e.preventDefault();
+    function handleSubmit(e) {
+      e.preventDefault();
 
-        }
+      // Get existing todos from local storage
+      const existingTodos = JSON.parse(localStorage.getItem('todos')) || [];
+
+      // Find the index of the current todo in the array
+      const index = existingTodos.findIndex(todo => todo.id === item.id);
+
+      // Update the todo with the new value
+      existingTodos[index] = {
+        ...existingTodos[index],
+        title: newValue
+      };
+
+      // Save the updated todos to local storage
+      localStorage.setItem('todos', JSON.stringify(existingTodos));
+
+
+    }
     function handleChange(e) {
-        const value= e.target.value;
-        setNewValue(value)
+      const value = e.target.value;
+      setNewValue(value)
     }
     function handleClick() {
-      onUpdate(item.id, newValue) 
-      setIsEdit(false) ;
+      onUpdate(item.id, newValue)
+      setIsEdit(false);
     }
 
     return (
-    <form className="todoUpdateForm" onSubmit={handleSubmit}>
-            <input type="text" className="todoInput" onChange={handleChange} value ={newValue}/>
-            <button className="button" onClick={handleClick}>Update</button>
-        </form>
+      <form className="todoUpdateForm" onSubmit={handleSubmit}>
+        <input type="text" className="todoInput" onChange={handleChange} value={newValue} />
+        <button className="button" onClick={handleClick}>Update</button>
+      </form>
     )
-    }
+  }
 
-function TodoElement() {
-        return (
-        
-        <div className="todoInfo">
+  function TodoElement() {
+    return (
+
+      <div className="todoInfo">
         <span className="todoTitle">{item.title}</span>
-        
-        <button className="button" onClick={()=> setIsEdit(true)}>Editar</button>
+
+        <button className="button" onClick={() => setIsEdit(true)}>Editar</button>
         <button className="buttonDelete" onClick={() => onDelete(item.id)}>Borrar</button>
       </div>
-        )
-    }
+    )
+  }
   return (
     <div className="todo">
-    {isEdit ? <FormEdit/> : <TodoElement/>} 
-    </div>  
+      {isEdit ? <FormEdit /> : <TodoElement />}
+    </div>
   );
 }
--- src/components/todoapp.js
+++ src/components/todoapp.js
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Todo from "./todo";
 import "./todoApp.css"
 
 export default function TodoApp() {
     const [title, setTitle] = useState("");
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState(() => {
+        const savedTodos = localStorage.getItem("todos");
+        if (savedTodos) {
+            return JSON.parse(savedTodos);
+        } else {
+            return [];
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -19,11 +31,11 @@ export default function TodoApp() {
         setTitle('')
 
     }
-    
-    function handleUpdate(id,value) {
-        const temp= [...todos];
-        const item= temp.find(item => item.id === id)
-        item.title= value;
+
+    function handleUpdate(id, value) {
+        const temp = [...todos];
+        const item = temp.find(item => item.id === id)
+        item.title = value;
         setTodos(temp)
     }
 
@@ -33,34 +45,34 @@ export default function TodoApp() {
         setTitle(value)
     }
 
-function handleDelete(id){
-    const temp= todos.filter(item => item.id !== id)
-    setTodos(temp)
+    function handleDelete(id) {
+        const temp = todos.filter(item => item.id !== id)
+        setTodos(temp)
 
-}
+    }
 
     return (
-    <div className="todoContainer">
-        
-        <form className="todoCreateForm" onSubmit={handleSubmit}>
-            <input onChange={handleChange} className="todoInput" value={title}/>
-            <input 
-            onClick={handleSubmit}
-            type='submit' 
-            value='Create todo' 
-            className="buttonCreate"/>
-            
-        </form>
+        <div className="todoContainer">
 
-        <div className="todosContainer">
-            {
-                todos.map(item => (
-                    <Todo key= {item.id} item={item} onUpdate={handleUpdate} onDelete={handleDelete}/> 
-                ))
-            }
+            <form className="todoCreateForm" onSubmit={handleSubmit}>
+                <input onChange={handleChange} className="todoInput" value={title} />
+                <input
+                    onClick={handleSubmit}
+                    type='submit'
+                    value='Create todo'
+                    className="buttonCreate" />
 
+            </form>
+
+            <div className="todosContainer">
+                {
+                    todos.map(item => (
+                        <Todo key={item.id} item={item} onUpdate={handleUpdate} onDelete={handleDelete} />
+                    ))
+                }
+
+            </div>
         </div>
-    </div>
     );
 
 }
